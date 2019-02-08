e03204f60dfcc5db23c955736b6eb79a8b962db4
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-02-08 12:25:20 -0600

    Avances

--- Tienda/WebForm2.aspx.vb
+++ Tienda/WebForm2.aspx.vb
@@ -1,6 +1,9 @@
 ﻿Public Class WebForm2
     Inherits System.Web.UI.Page
-    Dim Color As String
+    Dim ID As String
+    Dim ds As New DataSet
+    Dim da As OleDb.OleDbDataAdapter
+
 
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
@@ -8,12 +11,15 @@
     End Sub
 
     Protected Sub GridView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GridView1.SelectedIndexChanged
-        Color = GridView1.SelectedValue
-        MsgBox(Color)
+        ID = GridView1.SelectedValue
+        TextBox1.Text = Convert.ToString(ID)
     End Sub
 
     Protected Sub SqlDataSource1_Selecting(sender As Object, e As SqlDataSourceSelectingEventArgs) Handles SqlDataSource1.Selecting
 
     End Sub
 
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
