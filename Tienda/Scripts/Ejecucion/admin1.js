cfc1714f2308bb627e360b630febc50fc452e5ee
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-02-14 13:57:07 -0600

    Correciones falta validar aun

--- Tienda/Scripts/Ejecucion/admin1.js
+++ Tienda/Scripts/Ejecucion/admin1.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿
+$(document).ready(function () {
 
 
     $('#Alta').on('click', function () {//obtener datos cuando el periodo cambie
@@ -8,7 +9,7 @@
     var Precio = document.getElementById('Lacantidad').value;
     var Codigo = document.getElementById('Codigo').value;
     var Talla = "";
-
+        
     switch (true) {
         case $("#radio1").is(':checked'):
             Talla = "Chica";
@@ -25,42 +26,50 @@
         default:
             Talla = "Ninguno";
         }  
+        alert(Descrip.length);
+        if (Descrip.length == 0 || Cantida.length == 0 || Color.length == 0 || Precio.length == 0 || Codigo.length == 0 || Talla == "Ninguno")
+        {
+            $('#imgmodal').html('<img src="../Scripts/imagenes/pregunta.png" class="img-fluid" width="100" height="100" alt="Responsive image"/>');
+            $('#txtmodatitle').html("<strong style='vertical - align: middle;'> Falta informacion</strong>");
+            $('#texmodal').html("<strong style='vertical - align: middle;'> Faltan algunos campos por ingresar</strong>");
+        }
+        else
+        {
 
-     
+            $.ajax({
+                type: "POST",
+                url: "Admin1.aspx/altaproducto",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                async: false,
+                data: JSON.stringify({ Cantidad: Cantida, Codig: Codigo, Descrip: Descrip, Precio: Precio, Color: Color, Talla: Talla }),
+                success: function (result) {
+                    var valor = JSON.parse(result.d);
+                    var resultado = valor[0];
+                    alert(resultado);
+                    $('#myModal').modal({ show: true });
+                    if (resultado == "true") {
 
-                    $.ajax({
-                        type: "POST",
-                        url: "Admin1.aspx/altaproducto",
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json",
-                        async: false,
-                        data: JSON.stringify({ Cantidad: Cantida, Codig: Codigo, Descrip: Descrip, Precio: Precio, Color: Color, Talla: Talla }),
-                        success: function (result) {
-                            var valor = JSON.parse(result.d);
-                            var resultado = valor[0];
-                            alert(resultado);      
-                            $('#myModal').modal({ show: true });
-                            if (resultado == "true") {
-                    
-                                $('#imgmodal').html('<img src="../Scripts/imagenes/correcto.gif" class="img-fluid" width="100" height="100" alt="Responsive image"/>');
-                                $('#txtmodatitle').html("<strong style='vertical - align: middle;'> Alta exitoso</strong>");
-                                $('#texmodal').html("<strong style='vertical - align: middle;'> El produto: " + Descrip +" se dio de alta exitosamente </strong>");
-                            }
-                            else {
-                                $('#imgmodal').html('<img src="../Scripts/imagenes/alerta.gif" class="img-fluid" width="100" height="100" alt="Responsive image"/>');
-                                $('#txtmodatitle').html("<strong style='vertical - align: middle;'>Error de alta</strong>");
-                                $('#texmodal').html("<strong style='vertical - align: middle;'> El producto no se pudo ingresar, intente mas tarde </strong>");
-                            }
-                        },
-                        error: function (result) {
-                            console.log(result.responseText);
-                        }
+                        $('#imgmodal').html('<img src="../Scripts/imagenes/correcto.gif" class="img-fluid" width="100" height="100" alt="Responsive image"/>');
+                        $('#txtmodatitle').html("<strong style='vertical - align: middle;'> Alta exitoso</strong>");
+                        $('#texmodal').html("<strong style='vertical - align: middle;'> El produto " + Descrip + ", se dio de alta exitosamente </strong>");
+                    }
+                    else {
+                        $('#imgmodal').html('<img src="../Scripts/imagenes/alerta.gif" class="img-fluid" width="100" height="100" alt="Responsive image"/>');
+                        $('#txtmodatitle').html("<strong style='vertical - align: middle;'>Error de alta</strong>");
+                        $('#texmodal').html("<strong style='vertical - align: middle;'> El producto no se pudo ingresar, intente mas tarde </strong>");
+                    }
+                },
+                error: function (result) {
+                    console.log(result.responseText);
+                }
 
-                    }).done(function (data) {
-                        console.log(data);
-                    }).fail(function (data) {
-                        console.log("Error: " + data);
-                    });
+            }).done(function (data) {
+                console.log(data);
+            }).fail(function (data) {
+                console.log("Error: " + data);
+            });
+        }
 
     });
 
