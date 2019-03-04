b531bd9b928f205353bd0ce74058dd8ad6763c98
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-03-04 11:31:50 -0600

    Cambios de login boostrap

--- Tienda/Scripts/Plantilla/js/main.js
+++ Tienda/Scripts/Plantilla/js/main.js
@@ -2,21 +2,7 @@
 (function ($) {
     "use strict";
 
-
-    /*==================================================================
-    [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
-                showValidate(this);
-            }
-            else {
-                $(this).parent().addClass('true-validate');
-            }
-        })    
-    })
-  
-  
+    
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
@@ -38,11 +24,10 @@
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-           $(this).parent().removeClass('true-validate');
         });
     });
 
-     function validate (input) {
+    function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
@@ -59,22 +44,18 @@
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
-
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf136;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
-            });
-        });
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
+
         $(thisAlert).removeClass('alert-validate');
-        $(thisAlert).find('.btn-hide-validate').remove();
     }
+    
+    
 	
-	/*TRATANDO DE QUE EL MENU QUEDE FIJO*/
+	
+		/*TRATANDO DE QUE EL MENU QUEDE FIJO*/
 posicionarMenu();
 
 $(window).scroll(function() {    
@@ -93,10 +74,9 @@ function posicionarMenu() {
         $('.wrapper').css('margin-top', '0');
     }
 }
-    
-    
+	
+	
 
 })(jQuery);
-
 
 
