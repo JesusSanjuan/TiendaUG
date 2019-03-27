bac92f49cd56efc6762b92fa65c5fbaf8f705bde
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2019-03-27 14:13:20 -0600

    Avances

--- Tienda/Scripts/Ejecucion/LoadComplete_carrito.js
+++ Tienda/Scripts/Ejecucion/LoadComplete_carrito.js
@@ -1,6 +1,7 @@
 ﻿function operacion(Vectordeproductos, valorcarrito) {
+    alert(Vectordeproductos);
     var Valor = JSON.parse(JSON.stringify(valorcarrito));
-
+    var VectorProductos = JSON.parse(JSON.stringify(Vectordeproductos));
     if (Valor !== 0) {
         $("#contadorcompras").show();
         $('#contadorcompras').text(Valor);
--- Tienda/carrito.aspx.vb
+++ Tienda/carrito.aspx.vb
@@ -52,7 +52,6 @@ Public Class cart
         conectar()
         'Se genera el String de la Consulta
         Dim Contador As Integer = 0
-        Dim ResultadoFinal As Object = 0
         Dim consulta2 As String = "SELECT Cantidad_comprado FROM  carrito WHERE  carrito.Id_usuario =" & id_user_number
         'Agregamos la sentencia SQL y la conexion
         cmd = New SqlCommand(consulta2, conn)
@@ -69,7 +68,50 @@ Public Class cart
         cerrar()
         Dim ResultadoFinal2 As Object
         ResultadoFinal2 = JsonConvert.SerializeObject(sumacarrito)
-        Dim script As String = "operacion(" & ResultadoFinal & "," & ResultadoFinal2 & ");"
+
+
+        conectar()
+        ' MsgBox("INSERT INTO UG(Cantidad,Descripcion,Precio,Color,Talla,Id_imagen) VALUES('" + Cantidad + "','" + Descrip + "','" + Precio + "','" + Color + "','" + Talla + "','imagen_" + aStr + ".jpg')")
+        Dim consultaCan2 As String = "SELECT id_codigo_articulo, Cantidad_comprado FROM  carrito WHERE  carrito.Id_usuario =" & id_user_number & "and carrito.status_compra = 'carrito'"
+        cmd = New SqlCommand(consultaCan2, conn)
+        dr = cmd.ExecuteReader()
+        Dim res(1) As Object
+
+        If dr.HasRows Then
+            dr.Read()
+            res(0) = dr.GetValue(0)
+            res(1) = dr.GetValue(1)
+        End If
+        cerrar()
+        conectar()
+        Dim consulta As String = "SELECT Descripcion, Color, Talla, Precio, Codigo FROM  UG WHERE UG.Id_codigo =" & res(0)
+        'Agregamos la sentencia SQL y la conexion
+        cmd = New SqlCommand(consulta, conn)
+        dr = cmd.ExecuteReader()
+        Dim res2(4) As Object
+
+        If dr.HasRows Then
+            dr.Read()
+            res2(0) = dr.GetString(0)
+            res2(1) = dr.GetString(1)
+            res2(2) = dr.GetString(2)
+            res2(3) = dr.GetValue(3)
+            res2(4) = dr.GetString(4)
+        End If
+        cerrar()
+        Dim ResultadoFinal(6) As Object
+        ResultadoFinal(0) = res2(0)
+        ResultadoFinal(1) = res2(1)
+        ResultadoFinal(2) = res2(2)
+        ResultadoFinal(3) = res2(3)
+        ResultadoFinal(4) = res2(4)
+        ResultadoFinal(5) = res(1)
+        Dim ResultadoFinal3 As Object
+        ResultadoFinal3 = JsonConvert.SerializeObject(ResultadoFinal)
+
+
+
+        Dim script As String = "operacion(" & ResultadoFinal3 & "," & ResultadoFinal2 & ");"
         ScriptManager.RegisterStartupScript(Page, GetType(Page), "operacion", script, True)
     End Sub
 End Class
